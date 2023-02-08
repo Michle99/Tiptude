a836281b8a9f95d226aa33bc96f869aed188b1c8
Michle99 <sammiyonyx@gmail.com>
2023-02-07 23:15:05 -0500

    Initial commit

---
+++ app/src/main/java/com/maridev/tiptude/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.maridev.tiptude
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maridev.tiptude.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+            calculateTip()
+        }
+    }
+
+    private fun calculateTip() {
+        val stringTextField = binding.costOfService.text.toString()
+        val cost = stringTextField.toDoubleOrNull()
+        if(cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_fifteen_percent -> 0.15
+            R.id.option_ten_percent -> 0.10
+            else -> 0.05
+        }
+
+        var tip = tipPercentage * cost
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+}
